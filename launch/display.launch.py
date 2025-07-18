a70ab2aad4d376505e73a94f565542c60d268f80
YusukeKato <YusukeKato@users.noreply.github.com>
2025-07-18 15:21:52 +0900

    launchファイルのリファクタリング (#16)
    * Node()の引数の順番を統一
    
    * LaunchDescription()の書き方を統一
    
    * colcon testを通すために空行を追加
    
    * ruffで自動整形を実施

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -12,33 +12,38 @@ def generate_launch_description():
     declare_use_d435 = DeclareLaunchArgument(
         'use_d435',
         default_value='false',
-        description='Set true to attach the RealSense D435 model.'
+        description='Set true to attach the RealSense D435 model.',
     )
 
     description_loader = RobotDescriptionLoader()
     description_loader.use_d435 = LaunchConfiguration('use_d435')
 
-    rsp = Node(package='robot_state_publisher',
-               executable='robot_state_publisher',
-               output='both',
-               parameters=[{'robot_description': description_loader.load()}])
+    rsp = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=[{'robot_description': description_loader.load()}],
+    )
     jsp = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
-        output='screen')
+        output='screen',
+    )
 
-    rviz_config_file = get_package_share_directory(
-        'crane_x7_description') + '/launch/display.rviz'
-    rviz = Node(package='rviz2',
-                executable='rviz2',
-                name='rviz2',
-                output='log',
-                arguments=['-d', rviz_config_file])
+    rviz_config_file = get_package_share_directory('crane_x7_description') + '/launch/display.rviz'
+    rviz = Node(
+        name='rviz2',
+        package='rviz2',
+        executable='rviz2',
+        output='log',
+        arguments=['-d', rviz_config_file],
+    )
 
-    ld = LaunchDescription()
-    ld.add_action(declare_use_d435)
-    ld.add_action(rsp)
-    ld.add_action(jsp)
-    ld.add_action(rviz)
-
-    return ld
+    return LaunchDescription(
+        [
+            declare_use_d435,
+            rsp,
+            jsp,
+            rviz,
+        ]
+    )
